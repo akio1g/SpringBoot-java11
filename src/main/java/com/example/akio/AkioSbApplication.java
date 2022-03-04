0090fc4b8f89df68e056474243ccd8deb11dfcd4
Gabriel Akio <akioumebayashi@gmail.com>
2022-03-03 23:33:48 -0300

    Criação Projeto

---
+++ src/main/java/com/example/akio/AkioSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.akio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkioSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkioSbApplication.class, args);
+	}
+
+}
